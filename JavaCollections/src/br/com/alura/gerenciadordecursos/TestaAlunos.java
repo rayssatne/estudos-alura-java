022ba05c93037dcf38a2c2d67a961537e4560f1e
Rayssatnobrega <rayssa.nobrega@mat.ci.ufpb.br>
2021-08-30 12:16:08 -0300

    Adicionando os exercícios de java Collections

---
+++ JavaCollections/src/br/com/alura/gerenciadordecursos/TestaAlunos.java
@@ -0,0 +1,34 @@
+package br.com.alura.gerenciadordecursos;
+
+import java.util.*;
+
+public class TestaAlunos {
+    public static void main(String[] args) {
+        Collection<String> alunos = new HashSet<>();
+        alunos.add("Rodrigo Turini");
+        alunos.add("Alberto Souza");
+        alunos.add("Nico Steppat");
+        alunos.add("Sergio Lopes");
+        alunos.add("Renan Saggio");
+        alunos.add("Mauricio Aniche");
+        alunos.add("Alberto Souza");
+
+        boolean pauloEstaMatriculado = alunos.contains("Paulo Silveira");
+        alunos.remove("Sergio Lopes");
+        System.out.println(pauloEstaMatriculado);
+
+        System.out.println(alunos.size());
+
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
+        alunos.forEach(aluno -> {
+            System.out.println(aluno);
+        });
+
+        System.out.println(alunos);
+
+        List<String> alunosEmLista = new ArrayList<>(alunos);
+
+    }
+}
